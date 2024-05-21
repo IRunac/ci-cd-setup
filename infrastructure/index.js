0a8b4e07340b7bc9f5db37cce3ae8be247e3936f
Ivo Runac <ivo.runac@gmail.com>
2024-05-21 12:04:24 +0200

    Clean up and fix imageUri

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -8,12 +8,9 @@ const awsConfig = new pulumi.Config('aws');
 const irunacConfig = new pulumi.Config('irunac');
 const irunacAwsConfig = new pulumi.Config('irunacAws');
 
-const region = awsConfig.require('region');
 const domain = irunacConfig.require('domain');
-const PROJECT_NAME = irunacConfig.require('projectName');
 const hostedZoneId = irunacAwsConfig.require('hostedZoneId');
-// const ssmPrefix = irunacAwsConfig.require('ssmPrefix');
-const imageUri = '';
+const imageUri = process.env.SERVER_IMAGE;
 
 const frontend = {
   type: 'STATIC_SITE', serviceName: 'frontend', domain, hostedZoneId
